51d32344212b96cd76579ec43e11e3a6e9e93d8b
xCargan <lgc.carlier@gmail.com>
2023-06-20 21:06:12 +0200

    Update createExercice.js

--- frontend/src/pages/exercices/createExercice/createExercice.js
+++ frontend/src/pages/exercices/createExercice/createExercice.js
@@ -1,7 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-const createExercice = () => {
+const CreateExercice = () => {
+
+    const [selectedOption, setSelectedOption] = useState('');
+    const [colonne, setColonne] = useState(0);
+    const [ligne, setLigne] = useState(0);
+
+    useEffect(() => {
+
+    });
+
+    const handleSelectChange = (event) => {
+      setSelectedOption(event.target.value);
+    };
+
+    const RepeatVariable = () => {
+        const elements = [];
+        const numLignes = 5;
+        for (var i = 0; i < ligne; i++) {
+            for(var j = 0; i < colonne; j++) {
+                elements.push(
+                    <p>Element x</p>
+                )
+            }
+        }
+        return (
+            <div>
+              {elements}
+            </div>
+          );
+        }
 
     return (
         <div>
@@ -21,22 +50,35 @@ const createExercice = () => {
             </div>
             <div>
                 <p>Branche scolaire</p>
-                <select>
-                    <option>Mathématique</option>
-                    <option>Français</option>
-                    <option>Eveil</option>
-                    <option>Anglais</option>
-                    <option>Néérlandais</option>
+                <select value={selectedOption} onChange={handleSelectChange}>
+                    <option value="mathematique">Mathématique</option>
+                    <option value="francais">Français</option>
+                    <option value="eveil">Eveil</option>
+                    <option value="anglais">Anglais</option>
+                    <option value="neerlandais">Néérlandais</option>
                 </select>
-            </div>
-            <div>
-                <p>=============================================</p>
+                {selectedOption === 'mathematique' && 
+                    <div>
+                        <p>Contenu pour l'option mathematique</p>
+                        <div>
+                            <p>test</p>
+                        </div>
+                        <div>
+                            <RepeatVariable/>
+                        </div>
+                    </div>
+                }
+                {selectedOption === 'francais' && <div>Contenu pour l'option francais</div>}
+                {selectedOption === 'eveil' && <div>Contenu pour l'option eveil</div>}
+                {selectedOption === 'anglais' && <div>Contenu pour l'option anglais</div>}
+                {selectedOption === 'neerlandais' && <div>Contenu pour l'option neerlandais</div>}
             </div>
             <div>
                 <input type="text"></input>
             </div>
+
         </div>
     );
 };
 
-export default createExercice;
+export default CreateExercice;
