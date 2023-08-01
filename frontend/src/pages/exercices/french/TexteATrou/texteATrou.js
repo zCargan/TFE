348d258fc94be4ab856e6b02d2329b0fb03d8acb
xCargan <lgc.carlier@gmail.com>
2023-08-01 11:12:14 +0200

    travail sur le texte à trou

--- frontend/src/pages/exercices/french/TexteATrou/texteATrou.js
+++ frontend/src/pages/exercices/french/TexteATrou/texteATrou.js
@@ -1,34 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const texteATrou = () => {
+const TexteATrou = () => {
 
-    function ajouterMot() {
-        var textarea = document.getElementById("myTextarea");
-        var motAAjouter = "Nouveau mot ";
-      
-        // Check if the textarea element was found
-        if (textarea) {
-          // Récupérer le contenu actuel du textarea
-          var contenuActuel = textarea.value;
-      
-          // Ajouter le nouveau mot au contenu actuel
-          var nouveauContenu = contenuActuel + motAAjouter;
-      
-          // Mettre à jour le contenu du textarea avec le nouveau contenu
-          textarea.value = nouveauContenu;
+
+    const [addedWords, setAddedWords] = useState([])
+    const [phrases, setPhrases] = useState({})
+    const [currentSentence, setCurrentSentence] = useState("Texte à trou")
+    const [htmlSentence, setHTMLSentence] = useState("")
+
+    function addWords() {
+
+        var textarea = document.getElementById("textArea").value;
+        var motAAjouter = document.getElementById("motAAjouter").value;
+        let array = addedWords
+        array.push(motAAjouter)
+        setAddedWords(array)
+        var phrase = textarea + motAAjouter;
+        document.getElementById("textArea").value = phrase;
+        setCurrentSentence(phrase)
+
+        //setCurrentSentence(phraseHTML)
+
+    }
+
+    function validateSentence() {
+        if(document.getElementById("textArea").value === "") {
+            alert('Veuillez entrez une phrase valide!')
+        } else {
+
+            const nouveauDictionnaire = { ...phrases}
+            nouveauDictionnaire[document.getElementById("textArea").value] = addedWords;
+            setAddedWords([])
+            setPhrases(nouveauDictionnaire)
+
+            console.log(nouveauDictionnaire)
+
+            document.getElementById("textArea").value = "";
+            document.getElementById("motAAjouter").value = "";
+            alert("Phrases ajouté avec succès à l'exercice!")
+
         }
+    }
+
+    function changeValueText(event) {
+        let newSentence = document.getElementById("textArea").value
+        setCurrentSentence(newSentence)
     }
     
     
     return (
         <div>
-            <p>Texte à trou</p>
-            <textarea rows="5" cols="50" id="textArea"></textarea>
+            <p id="phrase">"Test"</p>
+            <textarea rows="5" cols="50" id="textArea" onChange={changeValueText}></textarea>
+            <br></br>
+            <button onClick={validateSentence}>Valider cette phrase</button>
             <p>Ajouter un mot?</p>
-            <input id="motAAjouter"></input><button onClick={ajouterMot}>Ajouter ce mot</button>
-            <p>testtest</p>
+            <input id="motAAjouter"></input><button onClick={addWords}>Ajouter ce mot</button>
+            <p>Liste des mots ajoutés : </p>
         </div>
     );
 };
 
-export default texteATrou;
+export default TexteATrou;
--- frontend/src/pages/exercices/french/french.js
+++ frontend/src/pages/exercices/french/french.js
@@ -11,7 +11,7 @@ const French = () => {
     };
 
     const divStyle = {
-        backgroundColor: 'red'
+        backgroundColor: 'lightblue'
       };
 
     return (
